e5303ee942ff11c6dc40ec1092175e308b849c9f
Ezequiel Alexandre <ezequielaraujo1997@gmail.com>
2017-06-14 19:06:24 -0300

    Console

---
+++ equacaoDoSegundo/src/equacaodosegundo/EquacaoDoSegundo.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package equacaodosegundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 1411120102
+ */
+public class EquacaoDoSegundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner (System.in);
+        
+        System.out.print("Digite o valor de A: ");
+        int a = teclado.nextInt();
+        
+        System.out.print("Digite o valor de B: ");
+        int b = teclado.nextInt();
+        
+        System.out.print("Digite o valor de C: ");
+        int c = teclado.nextInt();
+        
+        System.out.print("Valor de Delta é: ");
+        double d = Math.pow(b,2) - 4 * a * c;
+        
+        double x1 = (- b - Math.sqrt(d)) / (2 * a);
+        System.out.println("Valor de X linha é: " + x1);
+        
+        double x2 = (- b + Math.sqrt(d)) / (2 * a);
+        System.out.println("Valor de X duas linhas é: " + x2);
+        
+        if (d<0)
+            System.out.println("a equação não possui raízes reais.");
+    }
+    
+}
